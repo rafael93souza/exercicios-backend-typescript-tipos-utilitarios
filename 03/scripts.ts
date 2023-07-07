78f12a7cafba994940f962a9c8e2111cb1e86fa3
Rafael Souza <rafael93.souzasfc5@gmail.com>
2023-07-07 19:41:08 -0300

    :sparkles: feat: Exercise 03 finished

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -5,11 +5,23 @@ type Conn = {
     port: string,
     dbname: string
 }
+type ReturnConexao = {
+    driver: string,
+    url: string
+}
 
-const conexao = (dados: Conn) => {
+const conexao = (dados: Conn):Readonly<ReturnConexao>=> {
     const { username, password, host, port, dbname } = dados;
     return {
         driver: 'postgres',
         url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
     };
 }
+
+const conexaoDatabase = conexao({
+    host:"",
+    port:"", 
+    username:"", 
+    password:"", 
+    dbname:""
+});
